757216077f214a7c0574ca488c0a69b1da5b76ee
betchabippy <tullie@betchabippy.com>
2026-06-09 09:34:21 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'betchabippy-v25';
+const CACHE = 'betchabippy-v26';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
